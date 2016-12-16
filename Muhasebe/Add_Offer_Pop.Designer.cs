d715c8e65ce1c4dbb2f192f8423974cf71e2387f
panalgin <mashadow@outlook.com>
2016-12-16 19:17:28 +0200

    Package updates

--- Muhasebe/Add_Offer_Pop.Designer.cs
+++ Muhasebe/Add_Offer_Pop.Designer.cs
@@ -45,6 +45,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.Cancel_Button = new System.Windows.Forms.Button();
             this.Save_Button = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // Account_Box
@@ -63,24 +65,25 @@
             this.Attn_Note.Location = new System.Drawing.Point(427, 12);
             this.Attn_Note.Multiline = true;
             this.Attn_Note.Name = "Attn_Note";
-            this.Attn_Note.Size = new System.Drawing.Size(256, 47);
+            this.Attn_Note.Size = new System.Drawing.Size(273, 47);
             this.Attn_Note.TabIndex = 18;
-            this.Attn_Note.Text = "Aşağıda belirtmiş olduğumuz ürünler hakkında bilgi/fiyat talep ediyoruz.";
+            this.Attn_Note.Text = "Aşağıda belirtmiş olduğumuz ürünlerin birim ve toplam fiyatı bilgilerinize sunulm" +
+    "uştur.";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(352, 15);
+            this.label5.Location = new System.Drawing.Point(346, 15);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.Size = new System.Drawing.Size(75, 13);
             this.label5.TabIndex = 25;
-            this.label5.Text = "Yetkiliye Not:";
+            this.label5.Text = "Müşteriye Not:";
             // 
             // Delete_Button
             // 
             this.Delete_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Delete_Button.Enabled = false;
-            this.Delete_Button.Location = new System.Drawing.Point(608, 119);
+            this.Delete_Button.Location = new System.Drawing.Point(706, 119);
             this.Delete_Button.Name = "Delete_Button";
             this.Delete_Button.Size = new System.Drawing.Size(75, 23);
             this.Delete_Button.TabIndex = 20;
@@ -91,7 +94,7 @@
             // 
             this.Edit_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Edit_Button.Enabled = false;
-            this.Edit_Button.Location = new System.Drawing.Point(608, 90);
+            this.Edit_Button.Location = new System.Drawing.Point(706, 90);
             this.Edit_Button.Name = "Edit_Button";
             this.Edit_Button.Size = new System.Drawing.Size(75, 23);
             this.Edit_Button.TabIndex = 19;
@@ -108,26 +111,28 @@
             this.columnHeader7,
             this.columnHeader8,
             this.columnHeader9,
-            this.columnHeader10});
+            this.columnHeader10,
+            this.columnHeader1,
+            this.columnHeader2});
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(12, 90);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(590, 339);
+            this.listView1.Size = new System.Drawing.Size(688, 376);
             this.listView1.TabIndex = 24;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader6
             // 
-            this.columnHeader6.Text = "Ürün Resmi";
-            this.columnHeader6.Width = 68;
+            this.columnHeader6.Text = "Resim";
+            this.columnHeader6.Width = 42;
             // 
             // columnHeader7
             // 
-            this.columnHeader7.Text = "Ürün Kodu";
+            this.columnHeader7.Text = "Barkod";
             this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader7.Width = 87;
+            this.columnHeader7.Width = 76;
             // 
             // columnHeader8
             // 
@@ -137,15 +142,15 @@
             // 
             // columnHeader9
             // 
-            this.columnHeader9.Text = "İstediğimiz Adet";
+            this.columnHeader9.Text = "İstediğiz Miktar";
             this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader9.Width = 87;
+            this.columnHeader9.Width = 84;
             // 
             // columnHeader10
             // 
-            this.columnHeader10.Text = "Açıklama";
+            this.columnHeader10.Text = "Birim";
             this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.columnHeader10.Width = 195;
+            this.columnHeader10.Width = 63;
             // 
             // label4
             // 
@@ -175,7 +180,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(43, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 16;
@@ -186,7 +191,7 @@
             this.Cancel_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.Cancel_Button.Image = global::Muhasebe.Properties.Resources.cancel;
             this.Cancel_Button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Cancel_Button.Location = new System.Drawing.Point(608, 445);
+            this.Cancel_Button.Location = new System.Drawing.Point(706, 482);
             this.Cancel_Button.Name = "Cancel_Button";
             this.Cancel_Button.Size = new System.Drawing.Size(75, 23);
             this.Cancel_Button.TabIndex = 23;
@@ -198,18 +203,29 @@
             this.Save_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.Save_Button.Image = global::Muhasebe.Properties.Resources.tick;
             this.Save_Button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Save_Button.Location = new System.Drawing.Point(513, 445);
+            this.Save_Button.Location = new System.Drawing.Point(611, 482);
             this.Save_Button.Name = "Save_Button";
             this.Save_Button.Size = new System.Drawing.Size(89, 23);
             this.Save_Button.TabIndex = 21;
             this.Save_Button.Text = "Kaydet";
             this.Save_Button.UseVisualStyleBackColor = true;
             // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Toplam";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader1.Width = 72;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Açıklama";
+            this.columnHeader2.Width = 217;
+            // 
             // Add_Offer_Pop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 480);
+            this.ClientSize = new System.Drawing.Size(793, 517);
             this.Controls.Add(this.Account_Box);
             this.Controls.Add(this.Attn_Note);
             this.Controls.Add(this.label5);
@@ -252,5 +268,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox Name_Box;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
